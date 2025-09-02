8eb35dde9baba2a4a946199890cb2ff8f3d2bb2b
Théo <massa.theo@gmail.com>
2025-09-02 11:14:53 +0200

    update namespace

--- ublox_gps/launch/ublox_gps_node-launch.py
+++ ublox_gps/launch/ublox_gps_node-launch.py
@@ -44,8 +44,8 @@ def generate_launch_description():
         'config')
     ublox_gps_node = launch_ros.actions.Node(package='ublox_gps',
                                              executable='ublox_gps_node',
-                                             remappings=[('/ublox_gps_node/fix','/sensor/gps/fix'),
-                                                         ('ublox_gps_node/navpvt','/sensor/gps/navpvt')],
+                                             remappings=[('/ublox_gps_node/fix','/sensors/gps/fix'),
+                                                         ('ublox_gps_node/navpvt','/sensors/gps/navpvt')],
                                              output='both')
 
     return launch.LaunchDescription([ublox_gps_node,
